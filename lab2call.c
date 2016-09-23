65a0ae405079e31c008967e46f783cacd4727225
Alpr1010 <alpr1010@hotmail.com>
2016-09-23 16:18:44 -0400

    testing complete

--- lab2call.c
+++ lab2call.c
@@ -5,5 +5,5 @@ int main(void) {
     //Prints out number of sys calls made
     //why does the print statement need an int first according to user.h?
     printf(0, "Number of System Calls = %d\n", getcount());
-   // exit();
+    exit();
 }//end main
